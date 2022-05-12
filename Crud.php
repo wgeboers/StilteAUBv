07c6ff39cf842de422e8900adb39d06810d74a21
Bart Frijters <bjalfrij@avans.nl>
2022-05-12 12:32:40 +0200

    Update Crud.php
    beetje comments + ongebruikte waardes weg.

--- Crud.php
+++ Crud.php
@@ -4,12 +4,6 @@ require_once('db_conn.php');
 
 class Crud extends db_conn {
 	
-	public $cols=''; 
-    public $vals=''; 
-	
-    public $col=''; 
-    public $val=''; 
-	
 	public function __construct() {
 		parent::__construct();
 	}
@@ -20,26 +14,32 @@ class Crud extends db_conn {
 		return $select->fetchall(PDO::FETCH_OBJ);
 	}
 	
+	#used to add users to database
 	public function insertUser(array $data) {
 		
 		try {
-		$insert = $this->connection->prepare("INSERT INTO `users` (`First_Name`, `Last_Name`, `Email`, `Phone_Number`, `Password`, `Street`, `House_Number`, `Zipcode`, `City`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
-		$i = 0;
-		$j = 1;
+			$insert = $this->connection->prepare("INSERT INTO `users` (`First_Name`, `Last_Name`, `Email`, `Phone_Number`, `Password`, `Street`, `House_Number`, `Zipcode`, `City`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+			$i = 0;
+			$j = 1;
 		
-		while($i < count($data)) {
-			$insert->bindParam($j, $data[$i], PDO::PARAM_STR);
-			$i++;
-			$j++;
-		}
-		$insert->execute();
+			while($i < count($data)) {
+				$insert->bindParam($j, $data[$i], PDO::PARAM_STR);
+				$i++;
+				$j++;
+			}
 		} catch(PDOException $e) {
-			return $e;
-		}
+				return $e;
+			}
 		
 	}
 	
 	public function update($data, $table, $id) : void {
+		
+	}
+	
+	#TODO: generic insert into based on an array with key=>value pairs,
+	#means that all arrays need to have key=>value pairs, though. (Doable with forms, but that's later)...
+	public function insert($data, $table) {
 		
 	}
 	
