684b6cfdce7175c964cde1043d3714e01c6adfdc
Bart Frijters <bjalfrij@avans.nl>
2022-05-31 18:33:40 +0200

    unset objects that arent used.
    unset in header, because the objects are only used to get a user/employee's name from the database.

--- header.php
+++ header.php
@@ -13,6 +13,7 @@
 			$_SESSION['ErrorMsg'] = 'Wrong login';
 			unset($_SESSION['id']);
 		}
+		unset($e_man); //This object wont be used for anything but displaying a user's name.
 	} elseif(isset($_SESSION["id"]) && !empty($_SESSION["id"])) {
 		require_once("UserManager.php");
 		$u_man = new UserManager();
@@ -23,6 +24,7 @@
 			$_SESSION['ErrorMsg'] = 'Wrong login';
 			unset($_SESSION['id']);
 		}
+		unset($e_man); //This object wont be used for anything but displaying an employee's name.
 	}
 ?>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
