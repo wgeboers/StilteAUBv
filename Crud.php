ea26acd70f40e64c3e28be217a5e582487ef9b24
Bart Frijters <bjalfrij@avans.nl>
2022-06-02 17:34:38 +0200

    Deleted old files + refactor csv into crud/UserTest
    added UserTest which tests a user object.

--- Crud.php
+++ Crud.php
@@ -143,5 +143,32 @@ class Crud extends Database {
 			echo $e;
 		}
 	}
+
+	#NEEDS TO BE PUT ELSEWHERE.... EMPLOYEEMANAGER MAYBE?
+	function csvToDatabase($fp, $db) {
+		try {
+			$statement = $db->prepare("INSERT INTO `products` (`Name`, `Description`, `Stock`, `Price`, `Created_By`) VALUES (?, ?, ?, ?, ?)");
+			$i = 0;
+			
+			while(($data = fgetcsv($fp , 1000, ",")) !== FALSE){
+				
+				if($i >= 0) {
+					$data = str_replace('"', '', $data);
+					
+					$statement->bindParam(1, $data[0], PDO::PARAM_STR);
+					$statement->bindParam(2, $data[1], PDO::PARAM_STR);
+					$statement->bindParam(3, $data[2], PDO::PARAM_STR);
+					$statement->bindParam(4, $data[3], PDO::PARAM_STR);
+					$statement->bindParam(5, $data[4], PDO::PARAM_STR);
+					$statement->execute();
+				}
+				$i++;
+			}
+		}
+		catch(PDOException $e) {
+			echo $e->getMessage()."\n";
+		}
+		
+	}
 }
 ?>
