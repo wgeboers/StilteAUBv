388a697d1c819b03ad236e00e648c72a1c1c9547
ThomasD92 <91688880+ThomasD92@users.noreply.github.com>
2022-06-09 16:48:19 +0200

    Update Langmanager.php

--- Langmanager.php
+++ Langmanager.php
@@ -1,19 +1,48 @@
 <?php
-require_once ('Crud.php');
-$crud = new Crud ('root', '');
+
+require_once('Crud.php');
+
+ class LangManager {
+
+    private Crud $crud;
+    private $results =  array();
+
+    public function __construct() {
+        $this->crud = new Crud('root', '');
+    }
+
+    public function getContents($link) {
+        if($_SESSION['lang'] == 'lang_nl') {
+            $this->results = $this->crud->getLanguageContent('title_nl', 'text_nl', $link);
+        } elseif($_SESSION['lang'] === 'lang_en') {
+            $this->results = $this->crud->getLanguageContent('title_en', 'text_en', $link);
+        }
+
+        return $this->results;
+    }
+
+    public function getTexts() {
+        $contents = array();
+        foreach($this->results as $rows) {
+            foreach($rows as $key=>$value) {
+                if($key == 'text_nl' || $key == 'text_en')
+                    array_push($contents, $value);
+            }
+        }
+        return $contents;
+    }
+
+    public function getTitles() {
+        $titles = array();
+        foreach($this->results as $rows) {
+            foreach($rows as $key=>$value) {
+                if($key == 'title_nl' || $key == 'title_en')
+                    array_push($titles, $value);
+            }
+        }
+        return $titles;
+    }
+    
+
+ }
 ?>
-
-$langs = array('en','nl');
-
-<?php foreach ($langs as $lang): ?>
-    <a href="index.php?lang=<?=$lang;?>"><?=$lang;?> </a>
-<?php endforeach; ?>
-
-<?php
-if (in_array($_GET['lang'], $langs)){
-    $_SESSION['lang'] = $_GET['lang'];
-}
-?>
-
-
-
