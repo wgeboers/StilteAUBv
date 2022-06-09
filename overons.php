9970561c80b878335dd52f061421e456deca500d
wgeboers <wesleygeboers@outlook.com>
2022-06-09 21:15:30 +0200

    Update overons.php
    Over ons gereed kwa style

--- overons.php
+++ overons.php
@@ -21,33 +21,33 @@
 </head>
 
 <body class="overOns">
-    <div class="container-fluid text-white">
-        <div class="row">
-            <div class="col-xs-6 col-md-8 p-5 content">
-                <p> <?php echo $texts[0]; ?>
-                </p>
-                <p>
-                    <?php echo $texts[1]; ?>
-                </p>
-                <p>
-            </div>
-            <div class="col-xs-6 col-md-4 p-5 content">
-                <p>
-                    <img id="logoOverOns" src="Images/Logo.png" />
-                </p>
-                <p>
-                    NAAM: STILTE AUBV<br>
-                    ADRES: HOGESCHOOLLAAN 1<br>
-                    POSTCODE: 4818 CR<br>
-                    PLAATS: >BREDA<BR>
-                </p>
-                <p>
-                    TEL: 088 5698475<br>
-                    E-MAIL: info@stilteaubv.nl<br>
-                    KVK: 8978676748<br>
-                </p>
-            </div>
+    <div class="container">
+        <div class = "child">
+            <p> 
+                <?php echo $texts[0]; ?>
+            </p>
+            <p>
+                <?php echo $texts[1]; ?>
+            </p>
+            <p>
         </div>
+        <div class = "child">
+            <p>
+                <img id="logoOverOns" src="Images/Logo.png" />
+            </p>
+            <p>
+                <span class="overonsColorChange">NAAM: </span>STILTE AUBV<br>
+                <span class="overonsColorChange">ADRES: </span>HOGESCHOOLLAAN 1<br>
+                <span class="overonsColorChange">POSTCODE: </span>4818 CR<br>
+                <span class="overonsColorChange">PLAATS: </span>BREDA<BR>
+            </p>
+            <p>
+                <span class="overonsColorChange">TEL: </span>088 5698475<br>
+                <span class="overonsColorChange">E-MAIL: </span>info@stilteaubv.nl<br>
+                <span class="overonsColorChange">KVK: </span>8978676748<br>
+            </p>
+        </div>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ"
         crossorigin="anonymous"></script>
