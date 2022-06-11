ef26bb7c492f25b21aa7582a44c4a65acf31c280
Bart Frijters <bjalfrij@avans.nl>
2022-06-11 20:55:26 +0200

    getters + setters product/user

--- Product.php
+++ Product.php
@@ -24,30 +24,106 @@ class Product extends Image {
 	
 	public function toArray() : array {
 		return array('ID'=>$this->id, 'Name'=>$this->name, 'Desc'=>$this->desc, 'Stock'=>$this->stock, 'Price'=>$this->price, 'imageID'=>$this->imageID, 'imageName'=>$this->imageName, 'imageFilePath'=>$this->imageFilePath);
-	}
+	}   
 
-	public function getPrice() : float {
-		return $this->price;
-	}
-
-	public function getName() : string {
-		return $this->name;
-	}
-
-	public function getDescription() : string {
-		return $this->desc;
-	}
-
-	public function getProductID() : int {
+	/**
+	 * Get the value of id
+	 */ 
+	public function getId()
+	{
 		return $this->id;
 	}
 
-	public function getImagePath() : string {
-		return $this->imageFilePath;
+	/**
+	 * Set the value of id
+	 *
+	 * @return  self
+	 */ 
+	public function setId($id)
+	{
+		$this->id = $id;
+
+		return $this;
 	}
 
-	
+	/**
+	 * Get the value of name
+	 */ 
+	public function getName()
+	{
+		return $this->name;
+	}
 
-   
+	/**
+	 * Set the value of name
+	 *
+	 * @return  self
+	 */ 
+	public function setName($name)
+	{
+		$this->name = $name;
+
+		return $this;
+	}
+
+	/**
+	 * Get the value of desc
+	 */ 
+	public function getDesc()
+	{
+		return $this->desc;
+	}
+
+	/**
+	 * Set the value of desc
+	 *
+	 * @return  self
+	 */ 
+	public function setDesc($desc)
+	{
+		$this->desc = $desc;
+
+		return $this;
+	}
+
+	/**
+	 * Get the value of stock
+	 */ 
+	public function getStock()
+	{
+		return $this->stock;
+	}
+
+	/**
+	 * Set the value of stock
+	 *
+	 * @return  self
+	 */ 
+	public function setStock($stock)
+	{
+		$this->stock = $stock;
+
+		return $this;
+	}
+
+	/**
+	 * Get the value of price
+	 */ 
+	public function getPrice()
+	{
+		return $this->price;
+	}
+
+	/**
+	 * Set the value of price
+	 *
+	 * @return  self
+	 */ 
+	public function setPrice($price)
+	{
+		$this->price = $price;
+
+		return $this;
+	}
 }
 ?>
