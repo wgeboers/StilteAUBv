2861c791fe9e914fb7cf3cc624067d21792c188a
Bart Frijters <bjalfrij@avans.nl>
2022-06-12 21:25:34 +0200

    lang update sidenav

--- sidenav.php
+++ sidenav.php
@@ -1,5 +1,13 @@
 <!doctype HTML>
-
+<?php 
+if($_SESSION['lang'] === 'lang_nl') {
+        $infoTitle = 'Jouw gegevens';
+        $orderTitle = 'Jouw bestellingen';
+} elseif($_SESSION['lang'] === 'lang_en') {
+        $infoTitle = 'Your profile';
+        $orderTitle = 'Your orders';
+}
+?>
 <div class="sidenav">
         <?php if($_SESSION['type'] === 'employee') { ?>
                 <a href="employeesView.php">Medewerkers</a>
@@ -9,12 +17,9 @@
                 <a href="productsView.php">Artikelen</a>
                 <a href="imagesView.php">Afbeeldingen</a>
         <?php 
-        }
-        ?>
-        <?php if($_SESSION['type'] === 'user') { ?>
-                <a href='userInfo.php'>Jouw gegevens</a>
-                <a href='userOrders.php'>Jouw bestellingen</a>
-        <?php
+        } if($_SESSION['type'] === 'user') {
+                echo "<a href='userInfo.php'>{$infoTitle}</a>
+                <a href='userOrders.php'>{$orderTitle}</a>";
         }
         ?>
 </div>
